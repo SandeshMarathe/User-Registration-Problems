d63b73628ce2fb4e387a7b54e13524307c85a61c
SandeshMarathe <marathesandesh1999@gmail.com>
2022-01-08 16:28:22 +0530

    ADDED : Regex Problem For User Registration

---
+++ UserRegistration.java
@@ -0,0 +1,59 @@
+package com.bridgelabz.userregistration;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UserRegistration {
+
+    public static boolean isFirstName(String firstname) {
+        String  regex = "^[A-Z]{1}[a-z]{2,}";
+        Pattern patt = Pattern.compile(regex);
+        if (firstname == null) {
+            return false;
+        }
+        Matcher match = patt.matcher(firstname);
+        return match.matches();
+    }
+    public static boolean isLastName(String lastname) {
+        String regex = "^[A-Z]{1}[a-z]{2,}";
+        Pattern patt2 = Pattern.compile(regex);
+        if (lastname == null) {
+            return false;
+        }
+        Matcher match2 = patt2.matcher(lastname);
+        return match2.matches();
+    }
+    public static boolean isEmailValid(String emailid) {
+        String regex = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+        Pattern patt = Pattern.compile(regex);
+        if(emailid == null) {
+            return false;
+        }
+        Matcher match = patt.matcher(emailid);
+        return match.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter firstname:");
+        String firstname = input.nextLine();
+        System.out.println("Enter lastname:");
+        String lastname = input.nextLine();
+        System.out.println("Enter Email-ID :");
+        String emailid = input.nextLine();
+        if(isFirstName(firstname) == true) {
+            System.out.println("Firstname is Correct");
+        }else {
+            System.out.println("Firstname is Incorrect");
+        }
+        if(isLastName(lastname) == true) {
+            System.out.println("Lastname is Correct");
+        }else {
+            System.out.println("Lastname is Incorrect");
+        }
+        if(isEmailValid(emailid) == true) {
+            System.out.println("Email-ID is Correct");
+        }else {
+            System.out.println("Email-ID is Incorrect");
+        }
+    }
+}
